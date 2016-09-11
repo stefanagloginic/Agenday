68284460c88cb9fe5a54e98c5a0716f5040a4437
Stefana <stefana.gloginic@gmail.com>
2016-09-11 01:00:30 -0700

    Added tabs for the home screen that includes options home, profile and search.

--- app/src/main/java/com/example/stefana/agenday/HomeActivity.java
+++ app/src/main/java/com/example/stefana/agenday/HomeActivity.java
@@ -1,13 +1,34 @@
 package com.example.stefana.agenday;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new HomeFragment(), "Home");
+        viewPagerAdapter.addFragments(new ProfileFragment(), "Profile");
+        viewPagerAdapter.addFragments(new SearchFragment(), "Search");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+
 }
---
+++ app/src/main/java/com/example/stefana/agenday/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.stefana.agenday;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Stefana on 9/10/2016.
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment> fragments = new ArrayList<>();
+    ArrayList<String> tabTitles = new ArrayList<>();
+
+    public void addFragments(Fragment fragments, String titles){
+        this.fragments.add(fragments);
+        this.tabTitles.add(titles);
+    }
+    public ViewPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    public CharSequence getPageTitle(int position){
+        return tabTitles.get(position);
+    }
+}
